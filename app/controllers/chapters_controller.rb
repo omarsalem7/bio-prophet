30fae8d30601979b07cd33f8b3a5d15200bf6da7
Omar Salem <omarsalem0721@gmail.com>
2022-08-08 18:18:27 +0200

    Add authentication action for chapters controller

--- app/controllers/chapters_controller.rb
+++ app/controllers/chapters_controller.rb
@@ -1,4 +1,5 @@
 class ChaptersController < ApplicationController
+  before_action :authenticate_user!
   before_action :set_chapter, only: %i[ show edit update destroy ]
 
   # GET /chapters or /chapters.json
